393e50d00b1e143641677e5fa3dd7c9d9131cb6d
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2016-04-24 22:55:41 +0200

    Remove unnecessary directives imports

--- front/src/client/app/+todo/components/todo.list.component.ts
+++ front/src/client/app/+todo/components/todo.list.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from 'angular2/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import {TodoService} from '../services/todo.service';
 import {Todo} from '../models/todo.model';
 import {TodoNewComponent} from './todo.new.component';
@@ -10,7 +9,7 @@ import * as _ from 'lodash';
   selector: 'todo-list-cmp',
   templateUrl: 'app/+todo/components/todo.list.component.html',
   styleUrls: ['app/+todo/components/todo.list.component.css'],
-  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES, TodoNewComponent, TodoEntryComponent]
+  directives: [TodoNewComponent, TodoEntryComponent]
 })
 export class TodoListComponent implements OnInit {
     constructor(private todoService: TodoService) {}
@@ -18,10 +17,10 @@ export class TodoListComponent implements OnInit {
     todos: Todo[];
 
     ngOnInit() {
-    this.todoService.getTodos()
-        .subscribe((todos) => {
-          this.todos = todos;
-        });
+        this.todoService.getTodos()
+            .subscribe((todos) => {
+              this.todos = todos;
+            });
     }
 
     onAdd(todo: Todo) {
